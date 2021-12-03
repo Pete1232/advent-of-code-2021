9973dd6038252c57a15933b26e7a37ce50778775
petenewman <peter.newman@disneystreaming.com>
2021-12-03 21:46:53 +0000

    binary tests

--- app/test/src/utils/BinaryNumberTest.scala
+++ app/test/src/utils/BinaryNumberTest.scala
@@ -7,6 +7,21 @@ import math.Numeric.Implicits.infixNumericOps
 
 object DiagnosticReportTests extends TestSuite:
   val tests = Tests {
+    test("integer to binary") - {
+      assert(
+        Numeric[BinaryNumber].fromInt(22) == BinaryNumber(List(1, 0, 1, 1, 0))
+      )
+      println(Numeric[BinaryNumber].fromInt(9))
+      assert(
+        Numeric[BinaryNumber].fromInt(9) == BinaryNumber(List(1, 0, 0, 1))
+      )
+      assert(
+        Numeric[BinaryNumber].fromInt(2322) ==
+          BinaryNumber(
+            List(1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0)
+          )
+      )
+    }
     test("binary to integer") - {
       assert(BinaryNumber(List(1, 0, 1, 1, 0)).toInt == 22)
       assert(BinaryNumber(List(0, 1, 0, 0, 1)).toInt == 9)
