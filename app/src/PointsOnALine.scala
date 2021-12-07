77811db972c49dff61a46fa6bc86be7acf256483
petenewman <peter.newman@disneystreaming.com>
2021-12-07 18:16:03 +0000

    day 7 : 2, a bit slow

--- app/src/PointsOnALine.scala
+++ app/src/PointsOnALine.scala
@@ -1,8 +1,19 @@
 object PointsOnALine:
-  def closestPoint(positions: List[Int]): (Int, Int) =
-    List.tabulate(positions.max) { point =>
-      (point, distanceTo(point, positions))
-    }.minBy(_._2)
+  def closestPoint(positions: List[Int])(distanceFunction: (Int, Int) => Int): (Int, Int) =
+    List
+      .tabulate(positions.max) { point =>
+        (point, distanceTo(point, positions)(distanceFunction))
+      }
+      .minBy(_._2)
 
-  def distanceTo(point: Int, positions: List[Int]): Int =
-    positions.map(p => Math.abs(point - p)).sum
+  def distanceTo(point: Int, positions: List[Int])(
+      distanceFunction: (Int, Int) => Int
+  ): Int =
+    positions.map(p => distanceFunction(point, p)).sum
+
+  def simpleDistance(point1: Int, point2: Int): Int =
+    Math.abs(point1 - point2)
+
+  def complicatedDistance(point1: Int, point2: Int): Int =
+    val straightLineDistance = simpleDistance(point1, point2)
+    List.tabulate(straightLineDistance)(_ + 1).sum
--- app/test/src/PointsOnALineTests.scala
+++ app/test/src/PointsOnALineTests.scala
@@ -5,20 +5,42 @@ object PointsOnALineTests extends TestSuite:
   val tests = Tests {
     val positions = List(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
     test("calculate the closest position") - {
-      val count = PointsOnALine.closestPoint(positions)
+      val count =
+        PointsOnALine.closestPoint(positions)(PointsOnALine.simpleDistance)
       assert(count._1 == 2)
       assert(count._2 == 37)
     }
-    test("distance to point 1") - {
-      val count = PointsOnALine.distanceTo(1, positions)
+    test("simple distance to point 1") - {
+      val count =
+        PointsOnALine.distanceTo(1, positions)(PointsOnALine.simpleDistance)
       assert(count == 41)
     }
-    test("distance to point 3") - {
-      val count = PointsOnALine.distanceTo(3, positions)
+    test("simple distance to point 3") - {
+      val count =
+        PointsOnALine.distanceTo(3, positions)(PointsOnALine.simpleDistance)
       assert(count == 39)
     }
-    test("distance to point 10") - {
-      val count = PointsOnALine.distanceTo(10, positions)
+    test("simple distance to point 10") - {
+      val count =
+        PointsOnALine.distanceTo(10, positions)(PointsOnALine.simpleDistance)
       assert(count == 71)
+    }
+
+    test("complicated distance between two points") - {
+      assert(PointsOnALine.complicatedDistance(16, 5) == 66)
+      assert(PointsOnALine.complicatedDistance(1, 5) == 10)
+      assert(PointsOnALine.complicatedDistance(0, 5) == 15)
+    }
+    test("calculate the complicated closest position") - {
+      val count =
+        PointsOnALine.closestPoint(positions)(PointsOnALine.complicatedDistance)
+      assert(count._1 == 5)
+      assert(count._2 == 168)
+    }
+    test("complicated distance to point 2") - {
+      val count = PointsOnALine.distanceTo(2, positions)(
+        PointsOnALine.complicatedDistance
+      )
+      assert(count == 206)
     }
   }
