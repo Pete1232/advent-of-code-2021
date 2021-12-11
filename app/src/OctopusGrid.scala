90d043adf116340ab4b47983e86ded325872c01b
petenewman <peter.newman@disneystreaming.com>
2021-12-11 17:14:54 +0000

    day 11 part 1

---
+++ app/src/OctopusGrid.scala
@@ -0,0 +1,95 @@
+package utils
+
+import cats.Show
+
+case class OctopusGrid(
+    underlying: Map[(Int, Int), Int],
+    private val flashesUntilNow: Int
+):
+
+  lazy val nextStep: OctopusGrid =
+    OctopusGrid(nextStep(underlying.mapValues(_ + 1).toMap), totalFlashes)
+
+  // todo - redundant to iterate through again
+  lazy val flashes: Int = underlying.count(_._2 == 0)
+
+  lazy val totalFlashes: Int = flashesUntilNow + flashes
+
+  def step(count: Int): OctopusGrid =
+    if (count == 0)
+      this
+    else
+      nextStep.step(count - 1)
+
+  private def nextStep(grid: Map[(Int, Int), Int]): Map[(Int, Int), Int] =
+    val result =
+      grid.foldLeft(grid)((currentState, pointAndOldValue) =>
+        val currentPointValue = currentState(pointAndOldValue._1)
+        if (currentPointValue >= 10)
+          flash(pointAndOldValue._1, currentState)
+        else
+          currentState
+      )
+    if (result.eq(grid)) // intentionally checking reference equality
+      result
+    else
+      nextStep(result)
+
+  private def flash(
+      point: (Int, Int),
+      state: Map[(Int, Int), Int]
+  ): Map[(Int, Int), Int] =
+    val upLeft = (point._1 - 1, point._2 - 1)
+    val upMiddle = (point._1, point._2 - 1)
+    val upRight = (point._1 + 1, point._2 - 1)
+    val middleLeft = (point._1 - 1, point._2)
+    val middleRight = (point._1 + 1, point._2)
+    val downLeft = (point._1 - 1, point._2 + 1)
+    val downMiddle = (point._1, point._2 + 1)
+    val downRight = (point._1 + 1, point._2 + 1)
+
+    def inc(i: Int) =
+      if (i == 0) 0 else i + 1
+
+    state
+      .updatedWith(upLeft)(_.map(inc))
+      .updatedWith(upMiddle)(_.map(inc))
+      .updatedWith(upRight)(_.map(inc))
+      .updatedWith(middleLeft)(_.map(inc))
+      .updatedWith(middleRight)(_.map(inc))
+      .updatedWith(downLeft)(_.map(inc))
+      .updatedWith(downMiddle)(_.map(inc))
+      .updatedWith(downRight)(_.map(inc))
+      .updatedWith(point)(_.map(_ => 0))
+
+object OctopusGrid:
+  def buildGrid(s: String): OctopusGrid =
+    OctopusGrid(buildGrid(s, 0, 0, Map.empty), 0)
+
+  @scala.annotation.tailrec
+  private def buildGrid(
+      s: String,
+      x: Int,
+      y: Int,
+      result: Map[(Int, Int), Int]
+  ): Map[(Int, Int), Int] =
+    s.headOption match
+      case None =>
+        result
+      case Some(c) if c == '\n' =>
+        buildGrid(s.tail, 0, y + 1, result)
+      case Some(c) =>
+        buildGrid(
+          s.tail,
+          x + 1,
+          y,
+          result + ((x -> y) -> c.getNumericValue)
+        )
+
+  implicit val showGrid: Show[OctopusGrid] = Show.show(map =>
+    val maxX = map.underlying.maxBy(_._1._1)._1._1
+    val maxY = map.underlying.maxBy(_._1._2)._1._2
+    val grid =
+      List.tabulate(maxY + 1, maxX + 1)((y, x) => map.underlying((x, y)))
+    grid.map(_.mkString(" ")).mkString("\n")
+  )
---
+++ app/test/src/OctopusGridTests.scala
@@ -0,0 +1,72 @@
+import utest._
+import SyntaxChecker.LineType
+import utils.OctopusGrid
+import cats.implicits._
+
+object OctopusGridTests extends TestSuite:
+  val tests = Tests {
+    test("small example") - {
+      val grid = OctopusGrid.buildGrid(
+        """
+        |11111
+        |19991
+        |19191
+        |19991
+        |11111
+        """.stripMargin.trim
+      )
+      val result1 = grid.nextStep
+      val result2 = result1.nextStep
+      println(s"${grid.show}\n\n${result1.show}\n\n${result2.show}")
+      assert(result1.flashes == 9)
+      assert(result1.totalFlashes == 9)
+      assert(result2.flashes == 0)
+      assert(result1.totalFlashes == 9)
+    }
+
+    val testGrid = OctopusGrid.buildGrid(
+      """
+        |5483143223
+        |2745854711
+        |5264556173
+        |6141336146
+        |6357385478
+        |4167524645
+        |2176841721
+        |6882881134
+        |4846848554
+        |5283751526
+        """.stripMargin.trim
+    )
+    test("larger example") - {
+      val resultGrid10 = testGrid.step(10)
+      println(resultGrid10.show + "\n")
+      val result10 = resultGrid10.totalFlashes
+      assert(result10 == 204)
+
+      val resultGrid100 = testGrid.step(100)
+      println(resultGrid100.show)
+      val result100 = resultGrid100.totalFlashes
+      assert(result100 == 1656)
+    }
+
+    test("answer") - {
+      val input = OctopusGrid.buildGrid(
+        """
+        |7222221271
+        |6463754232
+        |3373484684
+        |4674461265
+        |1187834788
+        |1175316351
+        |8211411846
+        |4657828333
+        |5286325337
+        |5771324832
+        """.stripMargin.trim
+      )
+
+      val result = input.step(100).totalFlashes
+      assert(result == 1661)
+    }
+  }
