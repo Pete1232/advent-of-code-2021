30caea6472b799d7d0cd8fbdfd427e23b138ea12
petenewman <peter.newman@disneystreaming.com>
2021-12-14 10:31:32 +0000

    First pass on a tree search

---
+++ app/src/utils/Graph.scala
@@ -0,0 +1,17 @@
+package utils
+
+object Graph:
+
+  def search(vertex: Int, adjacencyMatrix: Grid): List[List[Int]] =
+    val adjacent = adjacencyMatrix.underlying.filter(entry => entry._1._1 == vertex && entry._2 >= 1).keySet.map(_._1).toList
+    adjacent.flatMap {  nextVertex =>
+        if(nextVertex == 1)
+          List(nextVertex +: List(1))
+        else
+          search(nextVertex, Grid(adjacencyMatrix.underlying.map( (entry, connections) =>
+            if(entry._1 == vertex)
+              entry -> (connections - 1)
+            else
+              entry -> connections
+          ))).map(nextVertex +: _)
+    }
