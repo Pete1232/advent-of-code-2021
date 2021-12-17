9091f38ca712c47f28d20a232e148175bd5c0c37
petenewman <peter.newman@disneystreaming.com>
2021-12-17 18:13:30 +0000

    finish day 17 part 1

--- app/src/TrickShot.scala
+++ app/src/TrickShot.scala
@@ -1,50 +1,3 @@
-object TrickShot:
-
-  def findBestShot(target: Target): Option[Int] =
-    val xRange = Range(0, target.maxXVelocity)
-    val yRange = Range(target.minYVelocity, target.maxYVelocity)
-
-    val maxHeights = yRange.map(y => y -> List.tabulate(y)(_ + 1).sum)
-
-    @scala.annotation.tailrec
-    def findXVelocity(yVelocity: Int, x: Int = 0): Option[Int] =
-      if(x < target.xMax)
-        step(
-          start = 0 -> 0,
-          xVelocity = x,
-          yVelocity = yVelocity
-        ) match {
-          case None =>
-            findXVelocity(x + 1, yVelocity)
-          case _ => Some(x)
-        }
-      else
-        None
-
-    @scala.annotation.tailrec
-    def step(start: (Int, Int), xVelocity: Int, yVelocity: Int): Option[(Int, Int)] =
-      val nextX = start._1 + xVelocity
-      val nextY = start._1 + yVelocity
-      if(xRange.contains(nextX) && yRange.contains(nextY))
-        Some(nextX -> nextY)
-      else if (nextX < target.xMax && nextY > target.yMin && xVelocity != 0)
-        step(
-          nextX -> nextY,
-          xVelocity = Math.max(xVelocity - 1, 0),
-          yVelocity = yVelocity - 1
-        )
-      else
-        None
-    
-    maxHeights.sortBy(_._2).reverse.foldLeft[Option[Int]](None)( (result, velocityAndHeight) =>
-      println(velocityAndHeight)
-      if(result.isDefined)
-        result
-      else
-        val (yVelocity, maxHeight) = (velocityAndHeight._1, velocityAndHeight._2)
-        findXVelocity(yVelocity = yVelocity).map(_ => maxHeight)
-    )
-  
 case class Target(xMin: Int, xMax: Int, yMin: Int, yMax: Int):
   require(xMin < xMax)
   require(yMin < yMax && yMax < 0)
@@ -55,3 +8,52 @@ case class Target(xMin: Int, xMax: Int, yMin: Int, yMax: Int):
 
   // more than this will overshoot the maximum x position
   val maxXVelocity: Int = xMax
+
+  // the target area
+  val xRange = Range.inclusive(xMin, xMax)
+  val yRange = Range.inclusive(yMin, yMax)
+
+  // take a trick shot at the target
+  // returns the point it hits the target area... if it does
+  @scala.annotation.tailrec
+  final def trickShot(start: (Int, Int), xVelocity: Int, yVelocity: Int): Option[(Int, Int)] =
+    val nextX = start._1 + xVelocity
+    val nextY = start._2 + yVelocity
+    if(xRange.contains(nextX) && yRange.contains(nextY))
+      Some(nextX -> nextY)
+    else if (nextX < xMax && nextY > yMin)
+      trickShot(
+        nextX -> nextY,
+        xVelocity = Math.max(xVelocity - 1, 0),
+        yVelocity = yVelocity - 1
+      )
+    else
+      None
+
+  // find the correct x velocity to make a trick shot for a given y velocity
+  @scala.annotation.tailrec
+  final def findXVelocity(xVelocity: Int = 0, yVelocity: Int): Option[Int] =
+    if(xVelocity < xMax)
+      trickShot(
+        start = 0 -> 0,
+        xVelocity = xVelocity,
+        yVelocity = yVelocity
+      ) match {
+        case None =>
+          findXVelocity(xVelocity + 1, yVelocity)
+        case _ => Some(xVelocity)
+      }
+    else
+      None
+
+  // returns the height of the shot that goes the highest
+  lazy val bestShot: Option[Int] =
+    val maxHeights = Range.inclusive(yMin, Math.abs(yMin)).map(y => y -> List.tabulate(y)(_ + 1).sum)
+
+    maxHeights.sortBy(_._2).reverse.foldLeft[Option[Int]](None)( (result, velocityAndHeight) =>
+      if(result.isDefined)
+        result
+      else
+        val (yVelocity, maxHeight) = (velocityAndHeight._1, velocityAndHeight._2)
+        findXVelocity(yVelocity = yVelocity).map(_ => maxHeight)
+    )
---
+++ app/test/src/TrickShotTests.scala
@@ -0,0 +1,50 @@
+import utest._
+
+object TrickShotTests extends TestSuite:
+  val tests = Tests {
+
+    val target = Target(20, 30, -10, -5)
+
+    test("(7,2) should hit the target") - {
+      val result = target.trickShot(
+        start = (0, 0),
+        xVelocity = 7,
+        yVelocity = 2
+      )
+      assert(result == Some(28 -> -7))
+
+      // (6,2) also hits, so this actually correctly returns 6
+      val calculatedXVelocity = target.findXVelocity(yVelocity = 2)
+      assert(calculatedXVelocity == Some(6))
+    }
+
+    test("(6,3) should hit the target") - {
+      val result = target.trickShot(
+        start = (0, 0),
+        xVelocity = 6,
+        yVelocity = 3
+      )
+      assert(result == Some(21 -> -9))
+
+      val calculatedXVelocity = target.findXVelocity(yVelocity = 3)
+      assert(calculatedXVelocity == Some(6))
+    }
+
+    test("(17,-4) should NOT hit the target") - {
+      val result = target.trickShot(
+        start = (0, 0),
+        xVelocity = 17,
+        yVelocity = -4
+      )
+      assert(result == None)
+
+      // a velocity of 11 will hit at (21,-9) before flying off into the distance
+      val calculatedXVelocity = target.findXVelocity(yVelocity = -4)
+      assert(calculatedXVelocity == Some(11))
+    }
+
+    test("find the maximum height that will hit the target") - {
+      val result = target.bestShot
+      assert(result == Some(45))
+    }
+  }
