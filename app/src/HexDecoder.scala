34e2672079a58ba8dfeb9ff21a3ea83f401318c4
petenewman <peter.newman@disneystreaming.com>
2021-12-17 21:24:15 +0000

    big refactor of packets, ready for sol_n

--- app/src/HexDecoder.scala
+++ app/src/HexDecoder.scala
@@ -1,38 +1,83 @@
 import utils.BinaryNumber
 import math.Numeric.Implicits.infixNumericOps
 
-case class Transmission(packet: Packet)
+trait Packet:
+  def version: Int
+  def typeId: Int
 
-case class Packet(version: String, typeId: String, content: String):
-  val decimalVersion: Int = BinaryNumber(version).toInt
-  val decimalTypeId: Int = BinaryNumber(typeId).toInt
+case class OperatorPacket(
+    version: Int,
+    typeId: Int,
+    lengthTypeId: Int,
+    subPackets: List[Packet]
+) extends Packet
 
-  val decodedContent: BinaryNumber =
-    BinaryNumber(
-      content
-        .grouped(5)
-        .foldLeft(("" -> false))((result, bits) =>
-          if (result._2)
-            result
+case class LiteralPacket(version: Int, typeId: Int, content: String)
+    extends Packet:
+  lazy val decimalValue = BinaryNumber(
+    content
+      .grouped(5)
+      .foldLeft(("" -> false))((result, bits) =>
+        if (result._2) result
+        else
+          val newResult = result._1 + bits.tail
+          if (bits.head == '0')
+            newResult -> true
           else
-            val newResult = result._1 + bits.tail
-            if (bits.head == '0')
-              newResult -> true
-            else
-              newResult -> false
-        )._1
-    )
+            newResult -> false
+      )
+      ._1
+  )
 
 object Packet:
-  def apply(hexString: String): Packet =
-    val binaryString = hexString.map(hexToBinary).mkString
-    Packet(
-      binaryString.take(3),
-      binaryString.drop(3).take(3),
-      binaryString.drop(6)
-    )
 
-  val hexToBinary = Map(
+  def fromHexString(hex: String): List[Packet] =
+    fromBinaryString(hex.map(hexToBinary).mkString)
+
+  def fromBinaryString(binary: String): List[Packet] =
+    val version = BinaryNumber(binary.take(3)).toInt
+    val typeId = BinaryNumber(binary.drop(3).take(3)).toInt
+    buildPackets(version, typeId, binary.drop(6))
+
+  def apply(version: Int, typeId: Int, content: String): Packet =
+    if (typeId == 4) LiteralPacket(version, typeId, content)
+    else
+      val lengthTypeId = content.head.toInt
+      if (lengthTypeId == 0)
+        // todo implement length limit
+        val length = BinaryNumber(content.tail.take(15)).toInt
+        OperatorPacket(
+          version,
+          typeId,
+          lengthTypeId,
+          fromBinaryString(content.drop(16))
+        )
+      else
+        // todo implement sub-packet count
+        OperatorPacket(
+          version,
+          typeId,
+          lengthTypeId,
+          fromBinaryString(content.drop(16))
+        )
+
+  private def buildPackets(
+      version: Int,
+      typeId: Int,
+      content: String,
+      packetString: String = ""
+  ): List[Packet] =
+    val chunk = content.take(5)
+    if (chunk.length < 5)
+      List(Packet(version, typeId, packetString + chunk))
+    else if (chunk.head == 0)
+      Packet(version, typeId, packetString + chunk) +: fromBinaryString(
+        content.drop(5)
+      )
+    else
+      buildPackets(version, typeId, content.drop(5), packetString + chunk)
+
+  private val hexToBinary = Map(
     '0' -> "0000",
     '1' -> "0001",
     '2' -> "0010",
--- app/test/src/HexDecoderTests.scala
+++ app/test/src/HexDecoderTests.scala
@@ -5,16 +5,11 @@ import math.Numeric.Implicits.infixNumericOps
 
 object HexDecoderTests extends TestSuite:
   val tests = Tests {
-    val testPacket = Packet("D2FE28")
+    val testPackets = Packet.fromHexString("D2FE28")
 
-    test("convert hex to binary") - {
-      val result = testPacket.version + testPacket.typeId + testPacket.content
-      assert(result == "110100101111111000101000")
-    }
     test("decode a literal value") - {
-      val result = testPacket.decodedContent
+      val result = testPackets.head.asInstanceOf[LiteralPacket].decimalValue
       assert(result == BinaryNumber("011111100101"))
       assert(result.toInt == 2021)
     }
   }
-  
